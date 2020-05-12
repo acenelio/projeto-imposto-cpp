bf46921f9a49635462792789631e1fe57be11ad2
Nelio Alves <acenelio@gmail.com>
2020-05-12 08:03:51 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double ganhoSalario, ganhoServicos, ganhoCapital, gastoMedico, gastoEducacao;
+
+    cout << "Renda anual com salario: ";
+    cin >> ganhoSalario;
+    cout << "Renda anual com prestacao de servico: ";
+    cin >> ganhoServicos;
+    cout << "Renda anual com ganho de capital: ";
+    cin >> ganhoCapital;
+    cout << "Gastos medicos: ";
+    cin >> gastoMedico;
+    cout << "Gastos educacionais: ";
+    cin >> gastoEducacao;
+
     return 0;
 }
